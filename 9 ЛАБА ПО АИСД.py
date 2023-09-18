307ad391b75a1ecd2ee6765465582370450a38d9
siiiu7 <124711298+siiiu7@users.noreply.github.com>
2023-09-18 20:40:58 +0400

    Update 9 ЛАБА ПО АИСД.py

--- 9 ЛАБА ПО АИСД.py
+++ 9 ЛАБА ПО АИСД.py
@@ -19,11 +19,21 @@ def open_welcome_window():
 
     welcome_window = Toplevel(window)
     welcome_window.title('Успешная регистрация')
-    welcome_window.geometry('400x300')
     welcome_window.config(bg='azure3')
 
     welcome_label = Label(welcome_window, text=f'Добро пожаловать, {name_value}!', font=f, bg='azure3')
     welcome_label.pack(pady=50)
+
+    # Вычисление позиции окна по середине экрана
+    window_width = welcome_window.winfo_width()
+    window_height = welcome_window.winfo_height()
+    screen_width = welcome_window.winfo_screenwidth()
+    screen_height = welcome_window.winfo_screenheight()
+    x_coordinate = int((screen_width / 2) - (window_width / 2))
+    y_coordinate = int((screen_height / 2) - (window_height / 2))
+
+    welcome_window.geometry(f'+{x_coordinate}+{y_coordinate}')
+
     welcome_window.transient(window)
     welcome_window.grab_set()
     window.wait_window(welcome_window)
@@ -31,7 +41,7 @@ def open_welcome_window():
 # Закрытие окна
 def on_close():
     if messagebox.askokcancel('Выход', 'Действительно хотите закрыть приложение?'):
-        window.quit()
+        window.destroy()
 
 window.protocol('WM_DELETE_WINDOW', on_close)
 
@@ -61,5 +71,16 @@ login_btn = Button(left_frame, width=15, text='Войти', font=f, relief=SOLID
 login_btn.grid(row=3, columnspan=2, pady=10)
 
 left_frame.pack()
+
+# Вычисление позиции окна по середине экрана
+window.update_idletasks()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x_coordinate = int((screen_width / 2) - (window_width / 2))
+y_coordinate = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f'+{x_coordinate}+{y_coordinate}')
 
 window.mainloop()
